3f252edc5f323acdba496b3583883bf86cde9c5e
KOS-noise <lkos.noisel@gmail.com>
2026-03-14 15:35:02 +0900

    entity를 생성하고 repository 인터페이스를생성

---
+++ src/main/java/com/rookies5/myspringbootlab/entity/Book.java
@@ -0,0 +1,32 @@
+package com.rookies5.myspringbootlab.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String isbn;
+    private Integer price;
+    private LocalDate publishDate;
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/rookies5/myspringbootlab/repository/BookRepositoryTest.java
@@ -0,0 +1,4 @@
+package com.rookies5.myspringbootlab.repository;
+
+public class BookRepositoryTest {
+}
