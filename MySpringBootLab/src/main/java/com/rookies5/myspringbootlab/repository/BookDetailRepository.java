d4620b66abc60d130fcaeda5305f4bb8285ecf2a
KOS-noise <lkos.noisel@gmail.com>
2026-03-21 12:57:28 +0900

    BookRepositoryTest 파일 생성

--- MySpringBootLab/src/main/java/com/rookies5/myspringbootlab/repository/BookDetailRepository.java
+++ MySpringBootLab/src/main/java/com/rookies5/myspringbootlab/repository/BookDetailRepository.java
@@ -1,2 +1,18 @@
 package com.rookies5.myspringbootlab.repository;
 
+import com.rookies5.myspringbootlab.entity.BookDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+import java.util.Optional;
+
+public interface BookDetailRepository extends JpaRepository <BookDetail, Long> {
+    @Query("SELECT bd FROM BookDetail bd WHERE bd.book.id = :bookId")
+    Optional<BookDetail> findByBookId(@Pram("bookId") Long bookId);
+
+    @Query("SELECT bd FROM BookDetail bd JOIN FETCH bd.book WHERE bd.id = :id")
+    Optional<BookDetail> findByIdWithBook(@Param("id") Long id);
+
+    List<BookDetail> findByPublisher(String publisher);
+}
